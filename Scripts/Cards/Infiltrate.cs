ec53f75a5befe77e6ed580f85d71bf44416c5eb7
SkyF-1 <3438254645@qq.com>
2026-04-26 00:04:57 +0800

    新增若干卡牌
    新增卡牌：渗透。

---
+++ Scripts/Cards/Infiltrate.cs
@@ -0,0 +1,55 @@
+using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.Helpers;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.Nodes.Rooms;
+using MegaCrit.Sts2.Core.Nodes.Vfx;
+using MegaCrit.Sts2.Core.Saves;
+using MegaCrit.Sts2.Core.Settings;
+using MegaCrit.Sts2.Core.HoverTips;
+using StsModBloodywolf.Scripts.Pools;
+using StsModBloodywolf.Scripts.Powers;
+using StsModBloodywolf.Scripts.DynamicVars;
+
+namespace StsModBloodywolf.Scripts.Cards;
+
+[Pool(typeof(BloodywolfCardPool))]
+public sealed class Infiltrate : CustomCardModel
+{/// 渗透
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => 
+    new List<IHoverTip>
+    {
+        HoverTipFactory.FromPower<VulnerablePower>()
+    };
+    public override IEnumerable<CardKeyword> CanonicalKeywords => new List<CardKeyword> { CardKeyword.Exhaust };
+    protected override IEnumerable<DynamicVar> CanonicalVars => new DynamicVar[]
+    {
+        new DynamicVar("perBlock", 2m),
+        new PowerVar<VulnerablePower>(1m)
+    };
+	public Infiltrate()
+		: base(0, CardType.Skill, CardRarity.Rare, TargetType.AnyEnemy)
+	{
+	}
+    public override string PortraitPath => $"res://StsModBloodywolf/images/cards/{Id.Entry.ToLowerInvariant()}.png";
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+		ArgumentNullException.ThrowIfNull(cardPlay.Target, "cardPlay.Target");
+        int blockAmount = cardPlay.Target.Block;
+        int Count = blockAmount / DynamicVars["perBlock"].IntValue;
+        for(int i = 0; i < Count; i++)
+        {
+            await PowerCmd.Apply<VulnerablePower>(cardPlay.Target, 1m, base.Owner.Creature, this);
+        }
+    }
+
+	protected override void OnUpgrade()
+	{
+		base.DynamicVars["perBlock"].UpgradeValueBy(-1m);
+	}
+}
